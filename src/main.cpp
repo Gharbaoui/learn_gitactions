1b3ef1c678523a0644439deae38fddd13aedd0f7
Gharbaoui <mohamed9gharbaoui@gmail.com>
2022-08-15 14:25:45 +0100

    main project

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "functions.hpp"
+
+int	main()
+{
+	std::cout << "addition o 2 and 3: " <<  add(2, 3) << "\n";
+	std::cout << "subtraction of 2 and 3: " <<  sub(2, 3) << "\n";
+}
+
