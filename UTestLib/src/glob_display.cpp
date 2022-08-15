50ad3ad0ea446c67974103991061e2ca5740ebe2
Gharbaoui <mohamed9gharbaoui@gmail.com>
2022-08-15 15:14:54 +0100

    instead of printing to stdout now to stderror

--- UTestLib/src/glob_display.cpp
+++ UTestLib/src/glob_display.cpp
@@ -2,13 +2,13 @@
 
 void	print(const std::string &str, unsigned line, const char *file)
 {
-	std::cout << file << ": " << line << "\n";
-	std::cout << str << "\n";
+	std::cerr << file << ": " << line << "\n";
+	std::cerr << str << "\n";
 }
 
 void	print_exit(const std::string &str, unsigned line, const char *file)
 {
-	std::cout << file << ": " << line << "\n";
-	std::cout << str << "\n";
+	std::cerr << file << ": " << line << "\n";
+	std::cerr << str << "\n";
 	exit(1);
 }
